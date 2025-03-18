ab3d8d908293d5a8d8f7a4632c14f2371d95cc4c
BaptisteCtldWbr <baptiste.catelandwambre@gmail.com>
2025-03-18 11:32:25 +0100

    Essai de corrections

--- app/routes/10DaysMeteo.tsx
+++ app/routes/10DaysMeteo.tsx
@@ -1,37 +1,46 @@
-/* 
-https://api.open-meteo.com/v1/forecast?latitude=48.8534&longitude=2.3488&hourly=temperature_2m&timezone=Europe%2FBerlin&past_days=5&forecast_days=1
-Only for non-commercial use and less than 10.000 daily API calls. See Terms for more details.
-*/
-
-
-import { json } from "@remix-run/node"; // or cloudflare/deno
+import * as React from 'react';
+import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
+import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
-import { DataGrid } from '@mui/x-data-grid';
+import type {} from '@mui/x-data-grid/themeAugmentation';
+
 
 export async function loader() {
-    const res = await fetch("https://api.open-meteo.com/v1/forecast?latitude=48.8534&longitude=2.3488&hourly=temperature_2m&models=meteofrance_seamless&temporal_resolution=hourly_6&start_date=2025-03-07&end_date=2025-03-17");
-    const data = await res.json();
-    return json(data);
+    try {
+        const res = await fetch("https://api.open-meteo.com/v1/forecast?latitude=48.8534&longitude=2.3488&hourly=temperature_2m&models=meteofrance_seamless&temporal_resolution=hourly_6&start_date=2025-03-07&end_date=2025-03-17");
+        const data = await res.json();
+        console.log(data);
+        return json(data);
+    } catch (error) {
+        console.error("Error fetching data:", error);
+        return json({ error: error.message });
+    }
 }
 
 export default function GistsRoute() {
-    const { hourly } = useLoaderData<typeof loader>();
+    const data = useLoaderData<typeof loader>();
+
+    const hourly = data.hourly;
+
     const { time, temperature_2m } = hourly;
 
+    
+    const rows = time.map((timeValue, index) => ({
+        id: index,
+        time: timeValue,
+        temperature: temperature_2m[index]
+    }));
+    console.log(rows);
+
+    const columns = [
+        { field: 'time', headerName: 'Jour', width: 200 },
+        { field: 'temperature', headerName: 'Température', width: 150 }
+    ];
+
     return (
-        <table>
-            <thead>
-                <th scope="col">Jour</th>
-                <th scope="col">Température</th>
-            </thead>
-            <ul>
-                {time.map((timeValue, index) => (
-                    <tr key={index}>
-                        <td>{timeValue}</td>
-                        <td>{temperature_2m[index]}</td>
-                    </tr>
-                ))}
-            </ul>
-        </table>
+        <div style={{ height: 300, width: '100%' }}>
+            <DataGrid rows={rows} columns={columns} />
+        </div>
+   
     );
 }
